e6d656a1aba3c2da55557984e8c424c177db2982
DDWayne <652507565@qq.com>
2021-07-23 10:35:37 +0800

    修改路径

---
+++ precache-manifest.f25286b47e4e1babf13cfa0f2b551720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ee954a7ec37a0e9d2223bd97b4809e",
+    "url": "/mumu-money-web-2/index.html"
+  },
+  {
+    "revision": "1fa6abb51a21bb55beff",
+    "url": "/mumu-money-web-2/static/css/main.47f2af02.chunk.css"
+  },
+  {
+    "revision": "8fef714923ee19343554",
+    "url": "/mumu-money-web-2/static/js/2.dd9fbe5e.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/mumu-money-web-2/static/js/2.dd9fbe5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa6abb51a21bb55beff",
+    "url": "/mumu-money-web-2/static/js/main.36fb5ca5.chunk.js"
+  },
+  {
+    "revision": "d24a2bb9dbb93c77e0ee",
+    "url": "/mumu-money-web-2/static/js/runtime-main.645e7b2e.js"
+  }
+]);
